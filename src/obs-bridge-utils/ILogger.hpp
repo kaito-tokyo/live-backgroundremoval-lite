09bfff87ae1d2a062ec3dbdfcfb7daaa11e9fd2d
Kaito Udagawa <umireon@gmail.com>
2025-09-14 07:59:42 +0900

    Update ILogger.hpp

--- src/obs-bridge-utils/ILogger.hpp
+++ src/obs-bridge-utils/ILogger.hpp
@@ -18,7 +18,12 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 
 #pragma once
 
+#include <cstdint>
+#include <cstdio>
 #include <string>
+#include <utility>
+
+#include <fmt/format.h>
 
 namespace kaito_tokyo {
 namespace obs_bridge_utils {
@@ -27,10 +32,51 @@ class ILogger {
 public:
 	virtual ~ILogger() noexcept = default;
 
-	virtual void debug(const std::string &message) const noexcept = 0;
-	virtual void info(const std::string &message) const noexcept = 0;
-	virtual void warn(const std::string &message) const noexcept = 0;
-	virtual void error(const std::string &message) const noexcept = 0;
+    template<typename... Args>
+    void debug(fmt::format_string<Args...> fmt, Args&&... args) const noexcept {
+        format_and_log(LogLevel::Debug, fmt, std::forward<Args>(args)...);
+    }
+
+    template<typename... Args>
+    void info(fmt::format_string<Args...> fmt, Args&&... args) const noexcept {
+        format_and_log(LogLevel::Info, fmt, std::forward<Args>(args)...);
+    }
+
+    template<typename... Args>
+    void warn(fmt::format_string<Args...> fmt, Args&&... args) const noexcept {
+        format_and_log(LogLevel::Warn, fmt, std::forward<Args>(args)...);
+    }
+
+    template<typename... Args>
+    void error(fmt::format_string<Args...> fmt, Args&&... args) const noexcept {
+        format_and_log(LogLevel::Error, fmt, std::forward<Args>(args)...);
+    }
+
+protected:
+    enum class LogLevel : std::int8_t {
+        Debug,
+        Info,
+        Warn,
+        Error
+    };
+
+    virtual void log(LogLevel level, const std::string& message) const noexcept = 0;
+    virtual LogLevel get_current_level() const noexcept {
+        return LogLevel::Debug;
+    }
+
+private:
+    template<typename... Args>
+    void format_and_log(LogLevel level, fmt::format_string<Args...> fmt, Args&&... args) const noexcept try {
+        if (level >= get_current_level()) {
+            const std::string message = fmt::format(fmt, std::forward<Args>(args)...);
+            log(level, message);
+        }
+    } catch (const std::exception& e) {
+        std::fprintf(std::stderr, "[LOGGER FATAL] Failed to format log message: %s\n", e.what());
+    } catch (...) {
+        std::fprintf(std::stderr, "[LOGGER FATAL] An unknown error occurred while formatting log message.\n");
+    }
 };
 
 } // namespace obs_bridge_utils
