fb98852e20195d9e9db23fa696cd84d79818e89c
Kaito Udagawa <umireon@kaito.tokyo>
2025-09-29 18:05:00 +0900

    Update SelfieSegmenter.cpp

--- src/SelfieSegmenter/SelfieSegmenter.cpp
+++ src/SelfieSegmenter/SelfieSegmenter.cpp
@@ -132,7 +132,6 @@ copyDataToMatAVX2(ncnn::Mat &inputMat, const std::uint8_t *bgra_data)
 	constexpr int PIXELS_PER_LOOP = 8;
 	const int num_loops = SelfieSegmenter::PIXEL_COUNT / PIXELS_PER_LOOP;
 
-	// 定数はループ外で定義
 	const __m256 v_inv_255 = _mm256_set1_ps(1.0f / 255.0f);
 	const __m256i mask_u8 = _mm256_set1_epi32(0x000000FF); // 各32-bit整数から下位8-bitを抽出するマスク
 
