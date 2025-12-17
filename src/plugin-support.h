9cf6c9ffcf0ddb7d2a75056c190e79f46a92ae5d
Kaito Udagawa <umireon@kaito.tokyo>
2025-12-18 03:45:53 +0900

    Refactor CI scripts and workflows, update configs
    Removed custom GitHub Actions and scripts for build, package, and formatting; replaced with new workflow files and simplified code signing setup. Updated .aiexclude and .gitignore for new structure, added .editorconfig, and reorganized CMake and resource files. Added new scripts for building and packaging on macOS and Windows, and made related updates to CMake and VSCode configuration files.

---
+++ src/plugin-support.h
@@ -0,0 +1,21 @@
+/*
+ * Live Background Removal Lite
+ * Copyright (C) 2025 Kaito Udagawa umireon@kaito.tokyo
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; for more details see the file
+ * "LICENSE.GPL-3.0-or-later" in the distribution root.
+ */
+
+#pragma once
+
+#define PLUGIN_NAME "live-backgroundremoval-lite"
+
+#ifndef PLUGIN_VERSION
+#define PLUGIN_VERSION "0.0.0"
+#endif
