d027b61134b4cba72dcfa893f9732424c24ee1c8
Kaito Udagawa <umireon@kaito.tokyo>
2025-12-19 17:43:42 +0900

    Update file verifier components and remove lock file
    Refactored DefineFileVerifierDropArea and FileVerifierResultDialog components, updated FileVerifierStore logic, and made adjustments to the versions page. Also removed the obsolete package-lock.json file from docs and updated package.json dependencies.

--- docs/src/components/FileVerifierResultDialog.svelte
+++ docs/src/components/FileVerifierResultDialog.svelte
@@ -23,28 +23,36 @@
 </script>
 
 <dialog {id}>
-  {#each $fileVerifiedResultStore as item}
-    {@const expectedAsset = findExpectedAsset(item.sha256)}
-    <dl>
-      {#if expectedAsset}
-        <dt>
-          <strong>{item.name}</strong>
-        </dt>
-        <dd>✅ File name on our server: {expectedAsset.name}</dd>
-        <dd>✅ Hash matched: {item.sha256}</dd>
-        {#if expectedAsset.size === item.size}
-          <dd>✅ File size matched: {item.size} bytes</dd>
+  <dl>
+    {#each $fileVerifiedResultStore as item}
+      {#if item.sha256}
+        {@const expectedAsset = findExpectedAsset(item.sha256)}
+        {#if expectedAsset}
+          <dt>
+            <strong>{item.name}</strong>
+          </dt>
+          <dd>✅ File name on our server: {expectedAsset.name}</dd>
+          <dd>✅ Hash matched: {item.sha256}</dd>
+          {#if expectedAsset.size === item.size}
+            <dd>✅ File size matched: {item.size} bytes</dd>
+          {:else}
+            <dd>
+              ❌ File size mismatch: expected {expectedAsset.size} bytes, got {item.size}
+              bytes
+            </dd>
+          {/if}
         {:else}
-          <dd>❌ File size mismatch: expected {expectedAsset.size} bytes, got {item.size} bytes</dd>
+          <dt><strong>{item.name}</strong></dt>
+          <dd>❌ Unknown file!</dd>
         {/if}
       {:else}
         <dt><strong>{item.name}</strong></dt>
-        <dd>❌ Unknown file!</dd>
+        <dd>❌ Verification failed: {item.error}</dd>
       {/if}
-    </dl>
-  {:else}
-    <p>No files received.</p>
-  {/each}
+    {:else}
+      <dt>No files received.</dt>
+    {/each}
+  </dl>
 
   <form method="dialog" style="text-align: right;">
     <button>Close</button>
