b1b79a8cc667784417ff4294e091aa5f0fd6e7b4
Kaito Udagawa <umireon@kaito.tokyo>
2025-12-24 18:07:05 +0900

    Update StartupController.cpp

--- src/LiveBackgroundRemovalLite/StartupUI/StartupController.cpp
+++ src/LiveBackgroundRemovalLite/StartupUI/StartupController.cpp
@@ -1,19 +1,7 @@
 /*
  * Live Background Removal Lite - StartupUI Module
  * Copyright (C) 2025 Kaito Udagawa umireon@kaito.tokyo
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * License: GNU GPL v3 or later
  */
 
 #include "StartupController.hpp"
@@ -64,11 +52,8 @@ void StartupController::showFirstRunDialog()
     // ---------------------------------------------------------
     QPalette pal = parent->palette();
 
-    // Get colors for injection into HTML
     QString colSubText = pal.color(QPalette::Disabled, QPalette::Text).name();
     QString colLink    = pal.color(QPalette::Active, QPalette::Link).name();
-
-    // CTA Text Color (Gold/Amber)
     const QString CTA_COLOR = "#ffb74d";
 
     // ---------------------------------------------------------
@@ -78,7 +63,8 @@ void StartupController::showFirstRunDialog()
     dialog->setAttribute(Qt::WA_DeleteOnClose);
     dialog->setWindowFlags(dialog->windowFlags() | Qt::WindowStaysOnTopHint | Qt::WindowCloseButtonHint | Qt::MSWindowsFixedSizeDialogHint);
 
-    dialog->setWindowTitle("Live Background Removal Lite - Installation Complete");
+    // タイトルを翻訳対応 (tr)
+    dialog->setWindowTitle(QObject::tr("Live Background Removal Lite - Installation Complete"));
 
     dialog->setStyleSheet(
         "QDialog {"
@@ -117,7 +103,8 @@ void StartupController::showFirstRunDialog()
     titleAreaLayout->setSpacing(2);
     titleAreaLayout->setAlignment(Qt::AlignVCenter | Qt::AlignLeft);
 
-    QLabel *titleLabel = new QLabel("Live Background Removal Lite", dialog);
+    // プラグイン名は固有名詞なのでtr不要かもしれませんが、念のため入れておきます
+    QLabel *titleLabel = new QLabel(QObject::tr("Live Background Removal Lite"), dialog);
     titleLabel->setStyleSheet("font-size: 24pt; font-weight: bold;");
     titleAreaLayout->addWidget(titleLabel);
 
@@ -125,13 +112,17 @@ void StartupController::showFirstRunDialog()
     QString currentVer = QString::fromStdString(globalContext_->pluginVersion_);
     QString latestVer  = QString::fromStdString(globalContext_->getLatestVersion());
 
+    // "Latest" などの単語を翻訳対応
     QString subtitleHtml = QString("<span style='font-size: 10pt; color: %1;'>v%2").arg(colSubText, currentVer);
-    if (!latestVer.isEmpty()) subtitleHtml += QString(" (Latest: %1)").arg(latestVer);
+    if (!latestVer.isEmpty()) {
+        subtitleHtml += QString(QObject::tr(" (Latest: %1)")).arg(latestVer);
+    }
     subtitleHtml += "</span>&nbsp;&nbsp;&nbsp;";
 
-    // Link
-    subtitleHtml += QString("<a href='%1' style='font-size: 10pt; color: %2; text-decoration: none;'>Official Site / Latest</a>")
-                    .arg(URL_OFFICIAL, colLink);
+    // リンクテキストを翻訳対応
+    QString linkText = QObject::tr("Official Site / Latest");
+    subtitleHtml += QString("<a href='%1' style='font-size: 10pt; color: %2; text-decoration: none;'>%3</a>")
+                    .arg(URL_OFFICIAL, colLink, linkText);
 
     QLabel *subtitleLabel = new QLabel(subtitleHtml, dialog);
     subtitleLabel->setOpenExternalLinks(true);
@@ -146,31 +137,34 @@ void StartupController::showFirstRunDialog()
     mainLayout->addWidget(line1);
 
     // ---------------------------------------------------------
-    // 4. Main Content
+    // 4. Main Content (Multi-language Support)
     // ---------------------------------------------------------
     QLabel *contentLabel = new QLabel(dialog);
     contentLabel->setWordWrap(true);
     contentLabel->setOpenExternalLinks(true);
 
-    QString contentHtml = R"(
-        <p style='font-size: 13px; margin-bottom: 5px;'>
-            Thank you for installing! 🎉<br>
-            Now, turn your room into a studio <b>without a green screen</b>.<br>
-            You are ready to create immersive streams.
-        </p>
-        <hr style='background-color: %1; height: 1px; border: none;'>
-        <p><b>[Quick Start]</b></p>
-        <ol style='line-height: 140%; margin-top: 0px; margin-bottom: 10px;'>
-            <li>Right-click your video source > <b>"Filters"</b></li>
-            <li>Click <b>[ + ]</b> and add <b>"Live Background Removal Lite"</b></li>
-        </ol>
-        <p style='margin-bottom: 5px;'>
-            <b>✨ Want better results?</b><br>
-            Check the <a href='%2' style='color: %3; text-decoration: none;'>Official Guide</a> for pro tips.
-        </p>
-    )";
+    // HTML全体をtrで囲みます。
+    // Raw String Literal (R"()") はやめて、通常の文字列結合にします。
+    // %1, %2 などのプレースホルダーは翻訳者が位置を変えられるようtrの中に含めます。
+    QString contentHtml = QObject::tr(
+        "<p style='font-size: 13px; margin-bottom: 5px;'>"
+        "Thank you for installing! 🎉<br>"
+        "Now, turn your room into a studio <b>without a green screen</b>.<br>"
+        "You are ready to create immersive streams."
+        "</p>"
+        "<hr style='background-color: %1; height: 1px; border: none;'>"
+        "<p><b>[Quick Start]</b></p>"
+        "<ol style='line-height: 140%; margin-top: 0px; margin-bottom: 10px;'>"
+        "<li>Right-click your video source > <b>\"Filters\"</b></li>"
+        "<li>Click <b>[ + ]</b> and add <b>\"Live Background Removal Lite\"</b></li>"
+        "</ol>"
+        "<p style='margin-bottom: 5px;'>"
+        "<b>✨ Want better results?</b><br>"
+        "Check the <a href='%2' style='color: %3; text-decoration: none;'>Official Guide</a> for pro tips."
+        "</p>"
+    ).arg(colSubText, URL_USAGE, colLink);
 
-    contentLabel->setText(contentHtml.arg(colSubText, URL_USAGE, colLink));
+    contentLabel->setText(contentHtml);
     mainLayout->addWidget(contentLabel);
 
     QFrame *line2 = new QFrame();
@@ -188,30 +182,30 @@ void StartupController::showFirstRunDialog()
     reviewLabel->setWordWrap(true);
     reviewLabel->setOpenExternalLinks(true);
 
-    QString reviewHtml = R"(
-        <p style='font-size: 12px; color: %1; margin: 0;'>
-            This plugin is developed by an individual.<br>
-            If you like it, a <b>5-star rating (★★★★★) on the forum</b><br>
-            would mean the world to the developer! 🚀
-        </p>
-	<p style='font-size: 13px;'>
-	    <a href='%2' style='color: %3; font-weight: bold;'>
-		▶ Click here to support with a review
-	    </a>
-	</p>
-    )";
-    reviewLabel->setText(reviewHtml.arg(colSubText, URL_FORUM, CTA_COLOR));
+    QString reviewHtml = QObject::tr(
+        "<p style='font-size: 12px; color: %1; margin: 0;'>"
+        "This plugin is developed by an individual.<br>"
+        "If you like it, a <b>5-star rating (★★★★★) on the forum</b><br>"
+        "would mean the world to the developer! 🚀"
+	"</p>"
+        "<p style='font-size: 13px;'>"
+        "<a href='%2' style='color: %3; font-weight: bold;'>"
+        "▶ Click here to support with a review"
+        "</a>"
+        "</p>"
+    ).arg(colSubText, URL_FORUM, CTA_COLOR);
+
+    reviewLabel->setText(reviewHtml);
     footerLayout->addWidget(reviewLabel, 1);
 
     // Close Button
-    QPushButton *closeBtn = new QPushButton("Close", dialog);
+    QPushButton *closeBtn = new QPushButton(QObject::tr("Close"), dialog);
     closeBtn->setCursor(Qt::PointingHandCursor);
     closeBtn->setDefault(true);
     closeBtn->setMinimumWidth(100);
     closeBtn->setMinimumHeight(36);
 
     QObject::connect(closeBtn, &QPushButton::clicked, dialog, &QDialog::close);
-
     footerLayout->addWidget(closeBtn, 0, Qt::AlignBottom);
 
     mainLayout->addLayout(footerLayout);
