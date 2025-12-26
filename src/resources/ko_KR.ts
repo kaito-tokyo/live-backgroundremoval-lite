dde79897e620ce240230d204ef5ed43fa8177608
Kaito Udagawa <umireon@kaito.tokyo>
2025-12-26 16:49:30 +0900

    Add Korean localization files and update resources
    Added Korean translation files (ko-KR.ini, ko_KR.ts, ko_KR.qm) for the Live Background Removal Lite plugin. Updated resources.qrc to include the new ko_KR.qm file, enabling Korean language support.

---
+++ src/resources/ko_KR.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR">
+<context>
+    <name>KaitoTokyo::LiveBackgroundRemovalLite::Global::PluginConfigDialog</name>
+    <message>
+        <location filename="../LiveBackgroundRemovalLite/Global/PluginConfigDialog.cpp" line="38"/>
+        <source>Live Background Removal Lite - Global Settings</source>
+        <translation>Live Background Removal Lite - 전역 설정</translation>
+    </message>
+    <message>
+        <location filename="../LiveBackgroundRemovalLite/Global/PluginConfigDialog.cpp" line="43"/>
+        <source>Updates</source>
+        <translation>업데이트</translation>
+    </message>
+    <message>
+        <location filename="../LiveBackgroundRemovalLite/Global/PluginConfigDialog.cpp" line="46"/>
+        <source>Check for updates automatically</source>
+        <translation>자동으로 업데이트 확인</translation>
+    </message>
+</context>
+<context>
+    <name>KaitoTokyo::LiveBackgroundRemovalLite::StartupUI::FirstRunDialog</name>
+    <message>
+        <location filename="../LiveBackgroundRemovalLite/StartupUI/FirstRunDialog.cpp" line="68"/>
+        <source>Live Background Removal Lite - Installation Complete</source>
+        <translation>Live Background Removal Lite - 설치 완료</translation>
+    </message>
+    <message>
+        <location filename="../LiveBackgroundRemovalLite/StartupUI/FirstRunDialog.cpp" line="105"/>
+        <source>Live Background Removal Lite</source>
+        <translation>Live Background Removal Lite</translation>
+    </message>
+    <message>
+        <location filename="../LiveBackgroundRemovalLite/StartupUI/FirstRunDialog.cpp" line="115"/>
+        <source> (Latest: %1)</source>
+        <translation> (최신: %1)</translation>
+    </message>
+    <message>
+        <location filename="../LiveBackgroundRemovalLite/StartupUI/FirstRunDialog.cpp" line="119"/>
+        <source>Official Site / Latest</source>
+        <translation>공식 사이트 / 최신 버전</translation>
+    </message>
+    <message>
+        <location filename="../LiveBackgroundRemovalLite/StartupUI/FirstRunDialog.cpp" line="143"/>
+        <source>&lt;p style=&apos;font-size: 13px; margin-bottom: 5px;&apos;&gt;Thank you for installing! 🎉&lt;br&gt;Now, turn your room into a studio &lt;b&gt;without a green screen&lt;/b&gt;.&lt;br&gt;You are ready to create immersive streams.&lt;/p&gt;&lt;hr style=&apos;background-color: %1; height: 1px; border: none;&apos;&gt;&lt;p style=&apos;font-size: 18px;&apos;&gt;&lt;b&gt;[Quick Start]&lt;/b&gt;&lt;/p&gt;&lt;ol style=&apos;line-height: 140%; margin-top: 0px; margin-bottom: 10px;&apos;&gt;&lt;li&gt;Right-click your video source &gt; &lt;b&gt;&quot;Filters&quot;&lt;/b&gt;&lt;/li&gt;&lt;li&gt;Click &lt;b&gt;[ + ]&lt;/b&gt; under Effect Filters and add &lt;b&gt;&quot;Live Background Removal Lite&quot;&lt;/b&gt;&lt;/li&gt;&lt;/ol&gt;&lt;p style=&apos;margin-bottom: 5px;&apos;&gt;&lt;b&gt;✨ Want better results?&lt;/b&gt;&lt;br&gt;Check the &lt;a href=&apos;%2&apos; style=&apos;color: %3;&apos;&gt;Official Guide&lt;/a&gt; for pro tips.&lt;/p&gt;</source>
+        <translation>&lt;p style=&apos;font-size: 13px; margin-bottom: 5px;&apos;&gt;설치해 주셔서 감사합니다! 🎉&lt;br&gt;이제 &lt;b&gt;그린 스크린 없이&lt;/b&gt; 여러분의 방을 스튜디오로 바꿔보세요.&lt;br&gt;몰입감 넘치는 방송을 시작할 준비가 되었습니다.&lt;/p&gt;&lt;hr style=&apos;background-color: %1; height: 1px; border: none;&apos;&gt;&lt;p style=&apos;font-size: 18px;&apos;&gt;&lt;b&gt;[빠른 시작]&lt;/b&gt;&lt;/p&gt;&lt;ol style=&apos;line-height: 140%; margin-top: 0px; margin-bottom: 10px;&apos;&gt;&lt;li&gt;비디오 소스를 마우스 우클릭 &gt; &lt;b&gt;&quot;필터&quot;&lt;/b&gt; 선택&lt;/li&gt;&lt;li&gt;효과 필터 아래의 &lt;b&gt;[ + ]&lt;/b&gt; 를 클릭하고 &lt;b&gt;&quot;Live Background Removal Lite&quot;&lt;/b&gt;를 추가하세요&lt;/li&gt;&lt;/ol&gt;&lt;p style=&apos;margin-bottom: 5px;&apos;&gt;&lt;b&gt;✨ 더 좋은 결과를 원하시나요?&lt;/b&gt;&lt;br&gt;프로 팁을 위해 &lt;a href=&apos;%2&apos; style=&apos;color: %3;&apos;&gt;공식 가이드&lt;/a&gt;를 확인해 보세요.&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../LiveBackgroundRemovalLite/StartupUI/FirstRunDialog.cpp" line="178"/>
+        <source>&lt;p style=&apos;font-size: 12px; color: %1; margin: 0;&apos;&gt;This plugin is developed by an individual.&lt;br&gt;If you like it, a &lt;b&gt;5-star rating (★★★★★) on the forum&lt;/b&gt;&lt;br&gt;would mean the world to the developer! 🚀&lt;/p&gt;&lt;p style=&apos;font-size: 13px;&apos;&gt;&lt;a href=&apos;%2&apos; style=&apos;color: %3; font-weight: bold;&apos;&gt;▶ Click here to support with a review&lt;/a&gt;&lt;/p&gt;</source>
+        <translation>&lt;p style=&apos;font-size: 12px; color: %1; margin: 0;&apos;&gt;이 플러그인은 개인이 개발하고 있습니다.&lt;br&gt;마음에 드신다면, &lt;b&gt;포럼에 별 5개(★★★★★) 평가&lt;/b&gt;를 남겨주세요.&lt;br&gt;개발자에게 정말 큰 힘이 됩니다! 🚀&lt;/p&gt;&lt;p style=&apos;font-size: 13px;&apos;&gt;&lt;a href=&apos;%2&apos; style=&apos;color: %3; font-weight: bold;&apos;&gt;▶ 리뷰로 응원하려면 여기를 클릭하세요&lt;/a&gt;&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../LiveBackgroundRemovalLite/StartupUI/FirstRunDialog.cpp" line="194"/>
+        <source>Close</source>
+        <translation>닫기</translation>
+    </message>
+</context>
+</TS>
