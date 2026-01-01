8944116106e3feb43e3b08468de4973498a4e926
Kaito Udagawa <umireon@kaito.tokyo>
2026-01-02 06:37:04 +0900

    Update license headers to SPDX format
    Replaced legacy copyright and license boilerplate with SPDX identifiers and standardized license text in all source and header files. Updated modules to use either GPL-3.0-or-later or MIT license as appropriate, improving clarity and compliance.

--- src/LiveBackgroundRemovalLite/Global/PluginConfigDialog.cpp
+++ src/LiveBackgroundRemovalLite/Global/PluginConfigDialog.cpp
@@ -1,15 +1,21 @@
 /*
- * Live Background Removal Lite - Global Module
- * Copyright (C) 2025 Kaito Udagawa umireon@kaito.tokyo
+ * SPDX-FileCopyrightText: Copyright (C) 2025 Kaito Udagawa umireon@kaito.tokyo
+ * SPDX-License-Identifier: GPL-3.0-or-later
  *
- * This program is free software; you can redistribute it and/or modify
+ * Live Background Removal Lite - Global Module
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; for more details see the file
- * "LICENSE.GPL-3.0-or-later" in the distribution root.
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "PluginConfigDialog.hpp"
