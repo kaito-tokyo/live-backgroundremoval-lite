d60841284edd38308d4cc4a91812476b169d34f1
Kaito Udagawa <umireon@kaito.tokyo>
2026-01-06 15:53:02 +0900

    Add multi-language support and version pages
    Expanded i18n-dic.json with translations for multiple languages and updated i18n.ts for consistent formatting. Added new pages for version history under docs/src/pages/versions, and updated layout and index pages to support language selection in the footer.

--- docs/src/lib/i18n.ts
+++ docs/src/lib/i18n.ts
@@ -1,6 +1,6 @@
-import i18nDic from './i18n-dic.json';
+import i18nDic from "./i18n-dic.json";
 
-export const defaultLang = 'en';
+export const defaultLang = "en";
 
 type I18nDic = typeof i18nDic;
 type Lang = keyof I18nDic;
@@ -9,9 +9,15 @@ type Keys = keyof I18nDic[typeof defaultLang];
 export const i18nLangs = Object.keys(i18nDic) as Lang[];
 
 export function useTranslations(lang: string) {
-  const validLang = (lang && lang in i18nDic) ? (lang as Lang) : defaultLang;
+  const validLang = lang && lang in i18nDic ? (lang as Lang) : defaultLang;
 
-  return (key: Keys) => {
-    return i18nDic[validLang][key] || i18nDic[defaultLang][key];
+  return (key: Keys, vars?: Record<string, string | number>) => {
+    let str = i18nDic[validLang][key] || i18nDic[defaultLang][key] || "";
+    if (vars) {
+      for (const [k, v] of Object.entries(vars)) {
+        str = str.replaceAll(`{${k}}`, String(v));
+      }
+    }
+    return str;
   };
 }
