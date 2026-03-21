d18c6b6e291e779450bc06d5529f429ab7d6d79f
Kaito Udagawa <umireon@kaito.tokyo>
2026-03-21 17:31:15 +0900

    Redirect en page to cannonical
    Signed-off-by: Kaito Udagawa <umireon@kaito.tokyo>

---
+++ pages/src/pages/en/index.html.js
@@ -0,0 +1,32 @@
+// SPDX-FileCopyrightText: 2025-2026 Kaito Udagawa <umireon@kaito.tokyo>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/// <reference types="astro/client" />
+
+import { withTrailingSlash } from "ufo";
+
+import { PRODUCTION_BASE_URL } from "../../lib/info.mjs";
+
+const { BASE_URL } = import.meta.env;
+
+const target = withTrailingSlash(BASE_URL);
+const cannonicalTarget = withTrailingSlash(PRODUCTION_BASE_URL);
+
+const html = `
+<!DOCTYPE html>
+<title>Redirecting...</title>
+<link rel="canonical" href="${cannonicalTarget}">
+<meta name="robots" content="noindex, nofollow">
+<meta http-equiv="refresh" content="0; url=${target}">
+<script>window.location.replace("${target}")</script>
+`;
+
+export async function GET() {
+  return new Response(html, {
+    status: 200,
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
+}
